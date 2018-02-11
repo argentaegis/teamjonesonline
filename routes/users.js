707fbc7ca1e9ad741942c3d8d80697ebcf3eacab
Andrew Jones <argentaegis@gmail.com>
2018-02-10 21:05:52 -0600

    cleanup

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,6 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
 
-
-
-
 // Register
 router.post('/register', (req, res, next) => {
   let newUser = new User({

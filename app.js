f7cba58b06ac3efa224b7b690700efd35614b0fb
Andrew Jones <argentaegis@gmail.com>
2018-02-14 11:56:40 -0600

    angular package updates

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const dbconfig = require('./config/database');
-const loadJsonFile = require('load-json-file');
-var http = require('http');
-var AWS = require('aws-sdk');
-
-const awsconfig = loadJsonFile.sync('./config/awsconfig.json');
-
-AWS.config.loadFromPath('./config/awsconfig.json');
-ddb = new AWS.DynamoDB({"endpoint": awsconfig.dynamoEndpoint, "region": awsconfig.region, "accessKeyId": awsconfig.accelerate, "secretAccessKey": awsconfig.secretAccessKey});
 
 
-const initTables = require('./models/initializeTables');
+console.log('DEBUG: ' + process.env.DEBUG);
+
+const dynamoose = require('dynamoose');
+
+if(process.env.DEBUG === 'true')
+{
+  dynamoose.local();
+}
 
 
 const app = express();
@@ -37,9 +37,9 @@ app.use('/users', users);
 
 
 app.get('/', (req, res) => {
-    res.send('index');
+  res.send('index');
 });
 
 app.listen(port, () => {
-    console.log('Server started  on port ' + port);
+  console.log('Server started  on port ' + port);
 });
